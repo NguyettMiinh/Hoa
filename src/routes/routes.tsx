f4af99295df4383903f8bac444a27891b545c9ca
NguyettMiinh <hinatavolshouyou@gmail.com>
2025-06-18 11:13:45 +0700

    add path register for signupform

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,13 @@
-import LoginForm from "../page/auth/LoginForm"
+import LoginForm from "../page/auth/LoginForm";
+import SignupForm from "../page/auth/SignupForm"
 const routes = [
     {
         path: "/login",
         element: <LoginForm />
+    },
+    {
+        path:"/register",
+        element: <SignupForm />
     }
 ]
 
